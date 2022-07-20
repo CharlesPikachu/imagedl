877f31e93a98abd68e03a573cda3ddeb9dcbc887
CharlesPikachu <1159254961@qq.com>
2022-07-20 23:10:01 +0800

    update docs

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     license=imagedl.__license__,
     include_package_data=True,
     entry_points={'console_scripts': ['imagedl = imagedl.imagedl:imagedlcmd']},
-    install_requires=list(open('requirements.txt', 'r').readlines()),
+    install_requires=[lab.strip('\n') for lab in list(open('requirements.txt', 'r').readlines())],
     zip_safe=True,
     packages=find_packages()
 )
