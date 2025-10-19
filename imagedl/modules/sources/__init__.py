99ceb41ec19e52f3503faae8792e54e4bfd12019
CharlesPikachu <1159254961@qq.com>
2025-10-20 05:59:16 +0800

    register google

--- imagedl/modules/sources/__init__.py
+++ imagedl/modules/sources/__init__.py
@@ -2,13 +2,14 @@
 from .base import BaseImageClient
 from .bing import BingImageClient
 from .baidu import BaiduImageClient
+from .google import GoogleImageClient
 from ..utils import BaseModuleBuilder
 
 
 '''ImageClientBuilder'''
 class ImageClientBuilder(BaseModuleBuilder):
     REGISTERED_MODULES = {
-        'BingImageClient': BingImageClient, 'BaiduImageClient': BaiduImageClient,
+        'BingImageClient': BingImageClient, 'BaiduImageClient': BaiduImageClient, 'GoogleImageClient': GoogleImageClient
     }
 
 
