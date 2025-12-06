bf2d590c121c81e4090e250fcab57c1a39a4ff01
CharlesPikachu <1159254961@qq.com>
2025-12-07 02:36:15 +0800

    update parse method

--- imagedl/modules/sources/__init__.py
+++ imagedl/modules/sources/__init__.py
@@ -4,6 +4,7 @@ from .bing import BingImageClient
 from .i360 import I360ImageClient
 from .baidu import BaiduImageClient
 from .sogou import SogouImageClient
+from .yahoo import YahooImageClient
 from .google import GoogleImageClient
 from ..utils import BaseModuleBuilder
 from .yandex import YandexImageClient
@@ -16,7 +17,7 @@ class ImageClientBuilder(BaseModuleBuilder):
     REGISTERED_MODULES = {
         'BingImageClient': BingImageClient, 'BaiduImageClient': BaiduImageClient, 'GoogleImageClient': GoogleImageClient, 
         'I360ImageClient': I360ImageClient, 'PixabayImageClient': PixabayImageClient, 'YandexImageClient': YandexImageClient,
-        'DuckduckgoImageClient': DuckduckgoImageClient, 'SogouImageClient': SogouImageClient,
+        'DuckduckgoImageClient': DuckduckgoImageClient, 'SogouImageClient': SogouImageClient, 'YahooImageClient': YahooImageClient,
     }
 
 
--- imagedl/modules/sources/yahoo.py
+++ imagedl/modules/sources/yahoo.py
@@ -6,10 +6,9 @@ Author:
 WeChat Official Account (微信公众号):
     Charles的皮卡丘
 '''
-import re
 import math
-from html import unescape
 from ..utils import Filter
+from bs4 import BeautifulSoup
 from urllib.parse import quote
 from .base import BaseImageClient
 
@@ -20,39 +19,64 @@ class YahooImageClient(BaseImageClient):
     def __init__(self, **kwargs):
         super(YahooImageClient, self).__init__(**kwargs)
         self.default_search_headers = {
-            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-            "Connection": "keep-alive",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-encoding": "gzip, deflate, br, zstd",
+            "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cache-control": "max-age=0",
+            "cookie": "GUC=AQEBCAFpK8ZpV0Ie7wSF&s=AQAAAAfiqwF_&g=aSp9bA; A1=d=AQABBDJQFWcCEMJwcR6f1oZWsTrFgsJ3yAkFEgEBCAHGK2lXadwr0iMA_eMDAAcIMlAVZ8J3yAk&S=AQAAAtxogq22gSrqBlASwUXzjM4; A3=d=AQABBDJQFWcCEMJwcR6f1oZWsTrFgsJ3yAkFEgEBCAHGK2lXadwr0iMA_eMDAAcIMlAVZ8J3yAk&S=AQAAAtxogq22gSrqBlASwUXzjM4; _ga=GA1.1.376091485.1764392311; axids=gam=y-gi4XaFlE2uIWDoRuMGBc84_CgmhvPV1W~A&dv360=eS1tMUJXZzI5RTJ1RW1hUXJrd3U4UWs0aC5MLmJsN2dQZX5B&ydsp=y-dzApDz5E2uLqPwEYzZSwx87F3DHhX2JO~A&tbla=y-YzJ9rXBE2uLHZ_QZLcZnJYpyLcPwzG3S~A; tbla_id=f7e07dff-176b-46ef-932e-9b1441f60525-tuct8b1b787; A1S=d=AQABBDJQFWcCEMJwcR6f1oZWsTrFgsJ3yAkFEgEBCAHGK2lXadwr0iMA_eMDAAcIMlAVZ8J3yAk&S=AQAAAtxogq22gSrqBlASwUXzjM4; cmp=t=1765034846&j=0&u=1---; gpp=DBAA; gpp_sid=-1; lkl=v=2&w=sFLKrWIPBmyF6MJjiEqG5ck2f.RU_PA6xW7o; ymuid=v=13B3697304196A5800087C6F05186BDB&ts=1765034898; _ga_B40QGCQW3G=GS2.1.s1765034845$o2$g1$t1765034909$j60$l0$h0; _ga_40HG6NTJFD=GS2.1.s1765043333$o3$g1$t1765045062$j47$l0$h0; yisn=lrs=1&=undefined&h=48&d=1521x346&dwh=1521x268",
+            "priority": "u=0, i",
+            "sec-ch-ua": "\"Chromium\";v=\"142\", \"Google Chrome\";v=\"142\", \"Not_A Brand\";v=\"99\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
         }
         self.default_headers = self.default_search_headers
         self._initsession()
     '''_parsesearchresult'''
     def _parsesearchresult(self, search_result: str):
-        image_infos, seen = [], set()
-        # extract from json
-        for m in re.finditer(r'"imgurl":"(https?://[^"]+)"', search_result):
-            url = m.group(1)
-            url = unescape(url)
-            url = url.replace("\\/", "/")
-            if url in seen: continue
-            seen.add(url)
-            image_infos.append({"candidate_urls": [url], "raw_data": {"imgurl": url}, "identifier": url})
-        # extract from <img src="...">
-        for m in re.finditer(r"<img[^>]+src=\"(https?://[^\" ]+)\"", search_result):
-            url = m.group(1)
-            url = unescape(url)
-            if url in seen: continue
-            seen.add(url)
-            image_infos.append({"candidate_urls": [url], "raw_data": {"src": url}, "identifier": url})
-        # return
+        soup = BeautifulSoup(search_result, "lxml")
+        image_infos = []
+        results_root = (soup.find("div", id="sres") or soup.find("div", id="results") or soup.find("div", {"class": "results"}) or soup)
+        def addimagefromnode(node, url_list):
+            urls = []
+            for u in url_list:
+                if not isinstance(u, str): continue
+                u = u.strip()
+                if not u: continue
+                if u.startswith("data:"): continue
+                urls.append(u)
+            if not urls: return
+            main_url = urls[0]
+            raw_data = {str(k).lower(): v for k, v in (node.attrs or {}).items()}
+            image_infos.append({"candidate_urls": urls, "raw_data": raw_data, "identifier": main_url})
+        for img in results_root.find_all("img"):
+            data_src = img.get("data-src")
+            src = img.get("src")
+            candidate_urls = []
+            if data_src: candidate_urls.append(data_src)
+            if src and src != data_src: candidate_urls.append(src)
+            if not candidate_urls: continue
+            flag_real = (
+                img.get("data-pos") is not None or any(key in (data_src or "") or key in (src or "") for key in ("sp.yimg.com/ib/th", "mm.bing.net", "yimg.com", "bing.net"))
+            )
+            if not flag_real: continue
+            addimagefromnode(img, candidate_urls)
+        for a in results_root.find_all("a", attrs={"data-src": True}):
+            data_src = a.get("data-src")
+            if not data_src: continue
+            addimagefromnode(a, [data_src])
         return image_infos
     '''_constructsearchurls'''
     def _constructsearchurls(self, keyword: str, search_limits: int = 1000, filters: dict = None, request_overrides: dict = None):
         request_overrides = request_overrides or {}
         base_url = "https://images.search.yahoo.com/search/images?p={keyword}&b={offset}"
         filter_str = self._getfilter().apply(filters, sep="&")
-        search_urls, page_size = [], 20
+        search_urls, page_size = [], 60
         for page in range(math.ceil(search_limits * 1.2 / page_size)):
             offset = page * page_size + 1
             search_url = base_url.format(keyword=quote(keyword), offset=offset)
