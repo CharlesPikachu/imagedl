ece5ea108d935e58dcd9f25008cfe5d4f5c0edc7
CharlesPikachu <1159254961@qq.com>
2026-02-12 04:43:00 +0800

    update daily check script

--- scripts/quick_check_imagedl.py
+++ scripts/quick_check_imagedl.py
@@ -16,7 +16,7 @@ from imagedl.modules import ImageClientBuilder, BaseImageClient
 
 
 '''settings'''
-QUERiES = ["Pikachu", "JK", "Cute Animals"]
+QUERiES = ["Pikachu", "JK", "Cute Animals", "Mountains"]
 MAX_SEARCH = 10
 MAX_DL_PER_CLIENT = 10
 RESULTS_ROOT = Path("daily_test_results")
@@ -182,6 +182,8 @@ def main():
         try:
             if client_name in SEARCH_SUPPLEMENT and runningingithubactions():
                 image_infos = SEARCH_SUPPLEMENT[client_name]
+            elif client_name in {'FreeNatureStockImageClient'}:
+                image_infos = client.search(QUERiES[3], search_limits=MAX_SEARCH, num_threadings=2)
             else:
                 image_infos = client.search(QUERiES[0], search_limits=MAX_SEARCH, num_threadings=2)
             for image_info in image_infos: image_info['raw_data'] = {}
