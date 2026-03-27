dbc046019a9732fe70603574531538d35261f0aa
CharlesPikachu <blwx96@connect.hku.hk>
2026-03-28 04:21:19 +0800

    refactor foodiesfeed and freenaturestock img client

--- imagedl/modules/sources/foodiesfeed.py
+++ imagedl/modules/sources/foodiesfeed.py
@@ -8,6 +8,7 @@ WeChat Official Account (微信公众号):
 '''
 import math
 import json_repair
+from ..utils import ImageInfo
 from .base import BaseImageClient
 from urllib.parse import quote, urlencode
 
@@ -17,43 +18,29 @@ class FoodiesfeedImageClient(BaseImageClient):
     source = 'FoodiesfeedImageClient'
     def __init__(self, **kwargs):
         super(FoodiesfeedImageClient, self).__init__(**kwargs)
-        self.default_search_headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
-        }
-        self.default_download_headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
-        }
+        self.default_search_headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
+        self.default_download_headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
         self.default_headers = self.default_search_headers
         self._initsession()
     '''_parsesearchresult'''
-    def _parsesearchresult(self, search_result: str):
+    def _parsesearchresult(self, search_result: str) -> list[ImageInfo]:
         # parse json text in safety
         search_result: dict = json_repair.loads(search_result)
         # parse search result
-        image_infos = []
-        for item in search_result.get('photos', []):
+        image_infos: list[ImageInfo] = []
+        for item in (search_result.get('photos', []) or []):
             if not isinstance(item, dict): continue
             candidate_urls = [item.get('master_url'), item.get('webp_url'), item.get('thumbnail_url')]
-            candidate_urls = [url for url in candidate_urls if url and str(url).startswith('http')]
-            candidate_urls = list(set(candidate_urls))
-            if not candidate_urls: continue
-            image_info = {
-                'candidate_urls': candidate_urls, 'raw_data': item, 'identifier': item['id'] if 'id' in item else candidate_urls[0],
-            }
-            image_infos.append(image_info)
+            if not (candidate_urls := list(set([url for url in candidate_urls if url and str(url).startswith('http')]))): continue
+            image_infos.append(ImageInfo(source=self.source, raw_data=item, candidate_download_urls=candidate_urls, identifier=item.get('id') or candidate_urls[0]))
         # return
         return image_infos
     '''_constructsearchurls'''
-    def _constructsearchurls(self, keyword, search_limits: int = 1000, filters: dict = None, request_overrides: dict = None):
-        request_overrides = request_overrides or {}
-        base_url = 'https://www.foodiesfeed.com/api/hybrid-photos?'
-        params = {'page': 1, 'limit': 24, 'locale': 'zh', 'sort': 'relevance', 'requireTagMatch': 'false', 'apiLocation': 'hybrid-search', 'localExhausted': 'true', 'istockOffset': 24, 'totalLoaded': 24, 'searchQuery': keyword}
-        if filters is not None: params.update(filters)
+    def _constructsearchurls(self, keyword: str, search_limits: int = 1000, filters: dict = None, request_overrides: dict = None):
+        request_overrides, filters, base_url = request_overrides or {}, filters or {}, 'https://www.foodiesfeed.com/api/hybrid-photos?'
+        (params := {'page': 1, 'limit': 24, 'locale': 'zh', 'sort': 'relevance', 'requireTagMatch': 'false', 'apiLocation': 'hybrid-search', 'localExhausted': 'true', 'istockOffset': 24, 'totalLoaded': 24, 'searchQuery': keyword}).update(filters)
         search_urls, page_size = [], int(params['limit'])
         for pn in range(math.ceil(search_limits * 1.2 / page_size)):
-            params['page'] = pn + 1
-            params['istockOffset'] = pn * page_size
-            params['totalLoaded'] = (pn + 1) * page_size
-            search_url = base_url + urlencode(params, quote_via=quote)
-            search_urls.append(search_url)
+            params['page'] = pn + 1; params['istockOffset'] = pn * page_size; params['totalLoaded'] = (pn + 1) * page_size
+            search_urls.append(base_url + urlencode(params, quote_via=quote))
         return search_urls
--- imagedl/modules/sources/freenaturestock.py
+++ imagedl/modules/sources/freenaturestock.py
@@ -8,6 +8,7 @@ WeChat Official Account (微信公众号):
 '''
 import math
 from bs4 import BeautifulSoup
+from ..utils import ImageInfo
 from urllib.parse import urljoin
 from .base import BaseImageClient
 
@@ -17,35 +18,26 @@ class FreeNatureStockImageClient(BaseImageClient):
     source = 'FreeNatureStockImageClient'
     def __init__(self, **kwargs):
         super(FreeNatureStockImageClient, self).__init__(**kwargs)
-        self.default_search_headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
-        }
-        self.default_download_headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
-        }
+        self.default_search_headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
+        self.default_download_headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
         self.default_headers = self.default_search_headers
         self._initsession()
     '''_parsesearchresult'''
-    def _parsesearchresult(self, search_result: str):
+    def _parsesearchresult(self, search_result: str) -> list[ImageInfo]:
         # parse with bs4
         soup, base_url = BeautifulSoup(search_result, "lxml"), "https://freenaturestock.com/"
-        urls = [urljoin(base_url, img["src"]) for img in soup.select("#photo-previews img[src]")]
+        urls = [urljoin(base_url, img["src"]) for img in soup.select("#photo-previews img[src]") if img and img.get('src')]
         # parse search result
-        image_infos = []
+        image_infos: list[ImageInfo] = []
         for item in urls:
             if not isinstance(item, str) or (not item.startswith('http')): continue
-            image_info = {
-                'candidate_urls': [item], 'raw_data': {}, 'identifier': item,
-            }
-            image_infos.append(image_info)
+            image_infos.append(ImageInfo(source=self.source, raw_data=search_result, candidate_download_urls=[item], identifier=item))
         # return
         return image_infos
     '''_constructsearchurls'''
-    def _constructsearchurls(self, keyword, search_limits: int = 1000, filters: dict = None, request_overrides: dict = None):
-        request_overrides = request_overrides or {}
+    def _constructsearchurls(self, keyword: str, search_limits: int = 1000, filters: dict = None, request_overrides: dict = None):
+        request_overrides, search_urls, page_size, filters = request_overrides or {}, [], 40, filters or {}
         base_url = 'https://freenaturestock.com/page/{pn}/?s={keyword}'
-        search_urls, page_size = [], 40
         for pn in range(math.ceil(search_limits * 1.2 / page_size)):
-            search_url = base_url.format(pn=pn, keyword=keyword)
-            search_urls.append(search_url)
+            search_urls.append(base_url.format(pn=pn, keyword=keyword))
         return search_urls
