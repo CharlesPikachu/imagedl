df12c465669bc08ecac197568eecb03f3b92fba8
CharlesPikachu <blwx96@connect.hku.hk>
2026-04-12 13:00:26 +0800

    upgrade search tips be consistent with musicdl

--- imagedl/modules/sources/base.py
+++ imagedl/modules/sources/base.py
@@ -146,11 +146,11 @@ class BaseImageClient():
         if owns_progress: main_process_context = Progress(TextColumn("{task.description}"), BarColumn(bar_width=None), MofNCompleteColumn(), TimeRemainingColumn(), refresh_per_second=10); main_process_context.__enter__()
         main_progress_lock = Lock() if main_progress_lock is None else main_progress_lock
         with main_progress_lock:
-            progress_id = main_process_context.add_task(f"{self.source}.search >>> Completed (0/{len(search_urls)}) URLs", total=len(search_urls))
+            progress_id = main_process_context.add_task(f"{self.source}.search >>> Completed (0/{len(search_urls)}) Search URLs", total=len(search_urls))
             if main_progress_id is not None:
                 cur_total = main_process_context.tasks[main_progress_id].total or 0
                 main_process_context.update(main_progress_id, total=cur_total + len(search_urls))
-                main_process_context.update(main_progress_id, description=f"Search From Sources >>> Completed ({int(main_process_context.tasks[main_progress_id].completed)}/{cur_total + len(search_urls)}) URLs")
+                main_process_context.update(main_progress_id, description=f"Search From Sources >>> Completed ({int(main_process_context.tasks[main_progress_id].completed)}/{cur_total + len(search_urls)}) Search URLs")
         submitted_tasks = []; image_infos: dict[str, list[ImageInfo]] = {}
         with ThreadPoolExecutor(max_workers=num_threadings) as pool:
             for search_url_idx, search_url in enumerate(search_urls): image_infos[str(search_url_idx)] = []; submitted_tasks.append(pool.submit(self._search, keyword, search_url, request_overrides, image_infos[str(search_url_idx)], main_process_context, progress_id))
@@ -158,9 +158,9 @@ class BaseImageClient():
                 future.result()
                 with main_progress_lock:
                     main_process_context.advance(progress_id, 1); num_searched_urls = int(main_process_context.tasks[progress_id].completed)
-                    main_process_context.update(progress_id, description=f"{self.source}.search >>> Completed ({num_searched_urls}/{len(search_urls)}) URLs")
+                    main_process_context.update(progress_id, description=f"{self.source}.search >>> Completed ({num_searched_urls}/{len(search_urls)}) Search URLs")
                     main_progress_id is not None and main_process_context.advance(main_progress_id, 1)
-                    main_progress_id is not None and main_process_context.update(main_progress_id, description=f"Search From Sources >>> Completed ({int(main_process_context.tasks[main_progress_id].completed)}/{int(main_process_context.tasks[main_progress_id].total or 0)}) URLs")
+                    main_progress_id is not None and main_process_context.update(main_progress_id, description=f"Search From Sources >>> Completed ({int(main_process_context.tasks[main_progress_id].completed)}/{int(main_process_context.tasks[main_progress_id].total or 0)}) Search URLs")
         image_infos = list(chain.from_iterable(image_infos.values())); image_infos = self._removeduplicates(image_infos=image_infos)
         image_infos: list[ImageInfo] = self._appenduniquefilepathforimages(image_infos=image_infos, keyword=keyword)
         # logging
