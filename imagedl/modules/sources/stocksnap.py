ce061736ccd1e25bb688373b6db913c3390386ac
CharlesPikachu <blwx96@connect.hku.hk>
2026-08-01 23:58:00 +0800

    fix stocksnap image client as the api changes

--- imagedl/modules/sources/stocksnap.py
+++ imagedl/modules/sources/stocksnap.py
@@ -6,12 +6,11 @@ Author:
 WeChat Official Account (微信公众号):
     Charles的皮卡丘
 '''
-import re
 import math
 import json_repair
 from ..utils import ImageInfo
+from urllib.parse import quote
 from .base import BaseImageClient
-from urllib.parse import quote, urlencode
 
 
 '''StockSnapImageClient'''
@@ -41,13 +40,7 @@ class StockSnapImageClient(BaseImageClient):
         return image_infos
     '''_constructsearchurls'''
     def _constructsearchurls(self, keyword: str, search_limits: int = 1000, filters: dict = None, request_overrides: dict = None):
-        request_overrides, base_url, filters = request_overrides or {}, 'https://api.gettyimages.com/v3/affiliates/search/images?', filters or {}
-        (resp := self.get(f'https://stocksnap.io/search/{keyword}', **request_overrides)).raise_for_status()
-        api_key, search_urls, page_size = re.search(r"\b(?:apiKey|api_key|API_KEY)\b\s*[:=]\s*['\"]([^'\"]+)['\"]", resp.text, re.I).group(1), [], 40
-        if 'Api-Key' not in self.default_headers: self.default_headers['Api-Key'] = api_key
-        if 'Api-Key' not in self.default_search_headers: self.default_search_headers['Api-Key'] = api_key
-        if 'Api-Key' not in self.default_download_headers: self.default_download_headers['Api-Key'] = api_key
-        search_urls.append(base_url + urlencode({'style': 'photography', 'phrase': keyword}, quote_via=quote))
+        search_urls, page_size, filters, request_overrides = [], 40, filters or {}, request_overrides or {}
         for pn in range(math.ceil(search_limits * 1.2 / page_size)):
             search_urls.append(f"https://stocksnap.io/api/search-photos/{quote(keyword, safe='')}/relevance/desc/{pn+1}")
         return search_urls
