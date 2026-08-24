931360796fb4192652fbe1cbcee7b195e77e5bb0
CharlesPikachu <blwx96@connect.hku.hk>
2026-08-25 04:26:59 +0800

    fix freenaturestock image client

--- imagedl/modules/sources/freenaturestock.py
+++ imagedl/modules/sources/freenaturestock.py
@@ -9,8 +9,8 @@ WeChat Official Account (微信公众号):
 import math
 from bs4 import BeautifulSoup
 from ..utils import ImageInfo
-from urllib.parse import urljoin
 from typing_extensions import Unpack
+from urllib.parse import urljoin, quote
 from .base import BaseImageClient, BaseImageClientKwargs
 
 
@@ -19,15 +19,15 @@ class FreeNatureStockImageClient(BaseImageClient):
     source = 'FreeNatureStockImageClient'
     def __init__(self, **kwargs: Unpack[BaseImageClientKwargs]):
         super(FreeNatureStockImageClient, self).__init__(**kwargs)
-        self.default_search_headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
-        self.default_download_headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
+        self.default_search_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36", "Referer": "https://freenaturestock.com/"}
+        self.default_download_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36", "Referer": "https://freenaturestock.com/"}
         self.default_headers = self.default_search_headers
         self._initsession()
     '''_parsesearchresult'''
     def _parsesearchresult(self, search_result: str) -> list[ImageInfo]:
         # parse with bs4
         soup, base_url = BeautifulSoup(search_result, "lxml"), "https://freenaturestock.com/"
-        urls = [urljoin(base_url, img["src"]) for img in soup.select("#photo-previews img[src]") if img and img.get('src')]
+        urls = [urljoin(base_url, img["src"]) for img in soup.select("#grid img[src]")]
         # parse search result
         image_infos: list[ImageInfo] = []
         for item in urls:
@@ -37,8 +37,8 @@ class FreeNatureStockImageClient(BaseImageClient):
         return image_infos
     '''_constructsearchurls'''
     def _constructsearchurls(self, keyword: str, search_limits: int = 1000, filters: dict = None, request_overrides: dict = None):
-        request_overrides, search_urls, page_size, filters = request_overrides or {}, [], 40, filters or {}
-        base_url = 'https://freenaturestock.com/page/{pn}/?s={keyword}'
+        request_overrides, search_urls, page_size, filters = request_overrides or {}, [], 48, filters or {}
+        base_url = 'https://freenaturestock.com/category/{keyword}/page/{pn}/'
         for pn in range(math.ceil(search_limits * 1.2 / page_size)):
-            search_urls.append(base_url.format(pn=pn, keyword=keyword))
+            search_urls.append(base_url.format(pn=pn+1, keyword=quote(keyword)) if pn > 0 else f'https://freenaturestock.com/category/{quote(keyword)}')
         return search_urls
